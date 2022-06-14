d5d78a49bff8a30df842a3d730e8a6d0c1cfeec3
caio <alcarriacaio@gmail.com>
2022-06-13 21:51:43 -0300

    versão 1

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = 'Caio'
+let sobrenome = 'Alcarria'
+let anoNascimento = 2003
+let cidade = 'Salvador'
+let idade = 2022-anoNascimento
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
